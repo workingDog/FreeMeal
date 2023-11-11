71d6397f76844df79b9f26dfad74570340706e49
workingdog <ringowathelet@gmail.com>
2023-11-11 14:57:55 +0900

    ingredients update

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 
+
 struct ContentView: View {
     @State var meals = [Meal]()
     
--- Shared/Meal.swift
+++ Shared/Meal.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 
+
 struct ApiResponse: Decodable {
     var meals: [Meal]?
 }
